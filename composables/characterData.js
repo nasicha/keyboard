c493dc627b31c07567e1f54010960e5778792ec0
Michael Tischler <michael.tischler@towa.at>
2023-07-06 09:21:43 +0200

    feat: change emoji icon

--- composables/characterData.js
+++ composables/characterData.js
@@ -1,8 +1,8 @@
 export default {
 	layouts: [
 		{state: 0, name: "default", value: "abc", shiftValue: "ABC", nextValue: "123" },
-		{state: 1, name: "symbols", value: "123", shiftValue: "?!%", nextValue: "☻" },
-    {state: 2, name: "emoji", value: "☻", shiftValue: "★", nextValue: "abc" },
+		{state: 1, name: "symbols", value: "123", shiftValue: "?!%", nextValue: "☺" },
+    {state: 2, name: "emoji", value: "☺", shiftValue: "★", nextValue: "abc" },
 	],
 	characterGroupsQWERTY: [
 		{
